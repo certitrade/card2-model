1e81fa468bbb313c1b0da36f2841268e7b63691e
Simon Mika <simon@mika.se>
2019-03-25 18:13:22 +0100

    Added missing exports.

--- source/index.ts
+++ source/index.ts
@@ -1,7 +1,8 @@
-import { Currency, CurrencyCode, Error, Language  } from "@certitrade/ct2-model"
+import { Currency, CurrencyCode, Error, Language, Timestamp  } from "@certitrade/ct2-model"
 import { Authorization } from "./Authorization"
 import * as Callback from "./Callback"
 import { Event } from "./Event"
+import { Merchant } from "./Merchant"
 import { Method } from "./Method"
 import { Payment } from "./Payment"
 
@@ -14,5 +15,7 @@ export {
 	Event,
 	Language,
 	Method,
+	Merchant,
 	Payment,
+	Timestamp,
 }
