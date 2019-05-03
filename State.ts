20445ec666e31bea648262e917fae43d57223072
Simon Mika <simon@mika.se>
2019-05-03 16:52:15 +0200

    Corrected state names.

--- State.ts
+++ State.ts
@@ -1,10 +1,7 @@
 export type State =
-	"INIT" |                         // Initialt state när Authorization objektet skapas
-	"READY_FOR_DOUBLE_CHECK" |       // Double-check innebär bortstädning av eventuella duplikata auktorisationer på en och samma payment
-	"READY_FOR_AUTH" |               // Redo för auktorisation via extern agent - Evry, Clearhaus etc.
-	"AUTH_IN_PROGRESS" |             // Auktorisation pågår
-	"AUTH_DONE" |                    // Auktorisation avslutad OK
-	"READY_FOR_CAPTURE" |            // Auktorisation avslutad OK och redo för capture
-	"CANCELED" |                     // Auktorisationen backad dvs cancellerad aka anullerad
-	"FAILED" |                       // Auktorisationen kunde ej genomföras
-	"AUTH_ERROR"                     // Auktorisationen kunde ej genomföras
+	"CREATED" |
+	"WAITING_FOR_AUTHORIZATION" |
+	"WAITING_FOR_CAPTURE" |
+	"CAPTURED" |
+	"FAILED" |
+	"CANCELLED"
